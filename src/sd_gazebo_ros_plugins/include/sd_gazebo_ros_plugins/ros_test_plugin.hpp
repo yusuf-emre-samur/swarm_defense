d4880ac03adf92dc135e19f9284b6aee137a4c59
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-02-17 15:39:38 +0100

    added basic and simple subscription

--- src/sd_gazebo_ros_plugins/include/sd_gazebo_ros_plugins/ros_test_plugin.hpp
+++ src/sd_gazebo_ros_plugins/include/sd_gazebo_ros_plugins/ros_test_plugin.hpp
@@ -3,8 +3,12 @@
 
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/gazebo.hh>
+
+// ros
 #include <gazebo_ros/node.hpp>
 #include <rclcpp/rclcpp.hpp>
+// msgs
+#include <geometry_msgs/msg/vector3.hpp>
 #include <std_msgs/msg/string.hpp>
 
 namespace sd {
@@ -20,16 +24,17 @@ class RosTestPlugin : public gazebo::ModelPlugin
 
 	void Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf);
 	void OnUpdate();
+	void topic_callback(geometry_msgs::msg::Vector3::SharedPtr msg);
 
   private:
 	// gazebo
-	gazebo::physics::ModelPtr model;
-	gazebo::event::ConnectionPtr updateConnection;
+	gazebo::physics::ModelPtr model_;
+	gazebo::event::ConnectionPtr updateConnection_;
 
 	// ros
 	gazebo_ros::Node::SharedPtr ros2node_;
 	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr subscription_;
 };
 } // namespace gazebo_ros_plugins
 } // namespace sd
--- src/sd_gazebo_ros_plugins/src/ros_test_plugin.cpp
+++ src/sd_gazebo_ros_plugins/src/ros_test_plugin.cpp
@@ -8,7 +8,11 @@
 #include <ignition/math/Vector3.hh>
 // ros
 #include <gazebo_ros/node.hpp>
+#include <gazebo_ros/utils.hpp>
 #include <rclcpp/rclcpp.hpp>
+// other
+#include <memory>
+#include <string>
 
 namespace sd {
 namespace gazebo_ros_plugins {
@@ -28,26 +32,29 @@ void RosTestPlugin::Load(gazebo::physics::ModelPtr _parent,
 						 sdf::ElementPtr _sdf)
 {
 	// Store the pointer to the model
-	this->model = _parent;
+	this->model_ = _parent;
 
+	// ros node + qos
 	ros2node_ = gazebo_ros::Node::Get(_sdf, _parent);
 	const gazebo_ros::QoS& qos = ros2node_->get_qos();
 
+	// publisher
 	publisher_ = ros2node_->create_publisher<std_msgs::msg::String>(
 		"test_topic", qos.get_publisher_qos("test_topic", rclcpp::QoS(10)));
 
-	// timer_ = ros2node_->create_wall_timer(
-	// 	500ms, std::bind(&RosTestPlugin::timer_callback, ros2node_.get()));
+	// subscription
+	subscription_ = ros2node_->create_subscription<geometry_msgs::msg::Vector3>(
+		"test_topic2", qos.get_subscription_qos("test_topic2", rclcpp::QoS(1)),
+		std::bind(&RosTestPlugin::topic_callback, this, std::placeholders::_1));
 
 	RCLCPP_INFO(ros2node_->get_logger(), "Ros Test Plugin Load");
 
 	// Listen to the update event. This event is broadcast every
 	// simulation iteration.
-	this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
+	this->updateConnection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
 		std::bind(&RosTestPlugin::OnUpdate, this));
 }
 
-// Called by the world update start event
 void RosTestPlugin::OnUpdate()
 {
 	auto msg = std_msgs::msg::String();
@@ -55,7 +62,14 @@ void RosTestPlugin::OnUpdate()
 	publisher_->publish(msg);
 
 	// Apply a small linear velocity to the model.
-	this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+	this->model_->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+}
+
+// Called by the world update start event
+void RosTestPlugin::topic_callback(
+	const geometry_msgs::msg::Vector3::SharedPtr msg)
+{
+	RCLCPP_INFO(ros2node_->get_logger(), std::to_string(msg->x).c_str());
 }
 
 // Register this plugin with the simulator
