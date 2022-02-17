12b04b9d215e1ffea22b9e03aac933f8fce932c9
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-02-17 21:45:26 +0100

    drone plugin

---
+++ src/sd_gazebo_ros_plugins/include/sd_gazebo_ros_plugins/sd_drone_plugin.hpp
@@ -0,0 +1,42 @@
+#ifndef SD_GAZEBO_ROS_PLUGINS_DRONE_PLUGIN_HPP_
+#define SD_GAZEBO_ROS_PLUGINS_DRONE_PLUGIN_HPP_
+
+// gazebo
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+// ros
+#include <gazebo_ros/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+// msgs
+#include <geometry_msgs/msg/vector3.hpp>
+#include <std_msgs/msg/string.hpp>
+
+namespace sd {
+namespace gazebo_ros_plugins {
+
+class DronePlugin : public gazebo::ModelPlugin
+{
+  public:
+	// Constructor
+	DronePlugin();
+
+	~DronePlugin();
+
+	void Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+	void OnUpdate();
+	void topic_callback(geometry_msgs::msg::Vector3::SharedPtr msg);
+
+  private:
+	// gazebo
+	gazebo::physics::ModelPtr model_;
+	gazebo::event::ConnectionPtr updateConnection_;
+
+	// ros
+	gazebo_ros::Node::SharedPtr ros2node_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr subscription_;
+};
+} // namespace gazebo_ros_plugins
+} // namespace sd
+
+#endif
---
+++ src/sd_gazebo_ros_plugins/src/sd_drone_plugin.cpp
@@ -0,0 +1,79 @@
+// interface
+#include "sd_gazebo_ros_plugins/sd_drone_plugin.hpp"
+// gazebo
+#include <functional>
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <ignition/math/Vector3.hh>
+// ros
+#include <gazebo_ros/node.hpp>
+#include <gazebo_ros/utils.hpp>
+#include <rclcpp/rclcpp.hpp>
+// other
+#include <memory>
+#include <string>
+
+namespace sd {
+namespace gazebo_ros_plugins {
+
+using namespace std::chrono_literals;
+
+// Constructor
+DronePlugin::DronePlugin()
+{
+}
+
+DronePlugin ::~DronePlugin()
+{
+}
+
+void DronePlugin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+	// Store the pointer to the model
+	this->model_ = _parent;
+
+	// ros node + qos
+	ros2node_ = gazebo_ros::Node::Get(_sdf, _parent);
+	const gazebo_ros::QoS& qos = ros2node_->get_qos();
+
+	// publisher
+	publisher_ = ros2node_->create_publisher<std_msgs::msg::String>(
+		"test_topic", qos.get_publisher_qos("test_topic", rclcpp::QoS(10)));
+
+	// subscription
+	subscription_ = ros2node_->create_subscription<geometry_msgs::msg::Vector3>(
+		"test_topic2", qos.get_subscription_qos("test_topic2", rclcpp::QoS(1)),
+		std::bind(&DronePlugin::topic_callback, this, std::placeholders::_1));
+
+	// Listen to the update event. This event is broadcast every
+	// simulation iteration.
+	this->updateConnection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
+		std::bind(&DronePlugin::OnUpdate, this));
+
+	// INFO
+	RCLCPP_INFO(ros2node_->get_logger(), "Loaded DronePlugin!");
+}
+
+// called each iteration of simulation
+void DronePlugin::OnUpdate()
+{
+	// create msg
+	auto msg = std_msgs::msg::String();
+	msg.data = "Hello World ";
+	// publish
+	publisher_->publish(msg);
+}
+
+// called each time receiving message from topic
+void DronePlugin::topic_callback(
+	const geometry_msgs::msg::Vector3::SharedPtr msg)
+{
+	RCLCPP_INFO(ros2node_->get_logger(), std::to_string(msg->x).c_str());
+}
+
+// Register this plugin
+GZ_REGISTER_MODEL_PLUGIN(DronePlugin)
+
+} // namespace gazebo_ros_plugins
+} // namespace sd
