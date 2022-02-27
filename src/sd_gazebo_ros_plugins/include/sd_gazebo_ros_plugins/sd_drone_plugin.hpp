243c6b790b7271056578b402f57a9ff32a1d4057
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-02-27 18:54:37 +0100

    imu sensor publisher

--- src/sd_gazebo_ros_plugins/include/sd_gazebo_ros_plugins/sd_drone_plugin.hpp
+++ src/sd_gazebo_ros_plugins/include/sd_gazebo_ros_plugins/sd_drone_plugin.hpp
@@ -7,6 +7,7 @@
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
+#include <gazebo/sensors/sensors.hh>
 #include <ignition/math/Pose3.hh>
 // ros
 #include <gazebo_ros/node.hpp>
@@ -14,6 +15,7 @@
 // msgs
 #include <sd_interfaces/msg/rotor_rpm.hpp>
 #include <sd_interfaces/msg/rpm.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <std_msgs/msg/string.hpp>
 
 namespace sd {
@@ -37,21 +39,29 @@ class DronePlugin : public gazebo::ModelPlugin
 	double calculateTorque(const double& w);
 	double rpm_to_rad_p_sec(const int& rpm);
 
+	// imu
+	void publish_imu_sensor();
+
   private:
 	// gazebo
 	gazebo::physics::ModelPtr model_;
 	gazebo::event::ConnectionPtr updateConnection_;
 	ignition::math::Pose3d pose_;
+	// imu sensor
+	gazebo::sensors::SensorPtr sensor_ptr_;
+	gazebo::sensors::ImuSensorPtr imu_sensor_ptr_;
+	rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_sensor_pub_;
 
 	// ros
 	gazebo_ros::Node::SharedPtr ros2node_;
-	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 	rclcpp::Subscription<sd_interfaces::msg::RotorRPM>::SharedPtr subscription_;
 
 	// params
 	uint num_rotors_;
 	std::vector<std::string> rotor_link_names_;
 	std::vector<int16_t> rotor_rpms_;
+	std::string imu_link_name_;
+	std::string imu_sensor_name_;
 
 	double rate_;
 	double rotor_thrust_coeff_;
--- src/sd_gazebo_ros_plugins/src/sd_drone_plugin.cpp
+++ src/sd_gazebo_ros_plugins/src/sd_drone_plugin.cpp
@@ -5,6 +5,7 @@
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <ignition/math/Quaternion.hh>
 #include <ignition/math/Vector3.hh>
 // ros
 #include <gazebo_ros/node.hpp>
@@ -34,17 +35,20 @@ void DronePlugin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
 	// Store the pointer to the model
 	this->model_ = _parent;
-
+	// ros node + qos
+	ros2node_ = gazebo_ros::Node::Get(_sdf, _parent);
+	const gazebo_ros::QoS& qos = ros2node_->get_qos();
+	// vars
 	this->rotor_thrust_coeff_ = 0.00025;
 	this->rotor_torque_coeff_ = 0.0000074;
-
-	// params
+	// rotor params
+	// num rotors
 	if ( _sdf->HasElement("num_rotors") ) {
 		this->num_rotors_ = _sdf->GetElement("num_rotors")->Get<uint>();
 	} else {
 		gzthrow("Please provide number of rotors !");
 	}
-	// for all rotors
+	// rotor link names
 	const std::string param_link_name_base = "link_name_rotor_";
 	std::string param_link_name;
 	for ( unsigned int i = 0; i < this->num_rotors_; i++ ) {
@@ -59,13 +63,36 @@ void DronePlugin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 		}
 	}
 
-	// ros node + qos
-	ros2node_ = gazebo_ros::Node::Get(_sdf, _parent);
-	const gazebo_ros::QoS& qos = ros2node_->get_qos();
-
-	// publisher
-	// publisher_ = ros2node_->create_publisher<std_msgs::msg::String>(
-	// 	"test_topic", qos.get_publisher_qos("test_topic", rclcpp::QoS(10)));
+	// imu sensor
+	// imu params
+	if ( _sdf->HasElement("imu_link_name") ) {
+		this->imu_link_name_ =
+			_sdf->GetElement("imu_link_name")->Get<std::string>();
+	} else {
+		gzthrow("Please provide imu_link_name");
+	}
+	if ( _sdf->HasElement("imu_sensor_name") ) {
+		this->imu_sensor_name_ =
+			_sdf->GetElement("imu_sensor_name")->Get<std::string>();
+	} else {
+		gzthrow("Please provide imu_sensor_name");
+	}
+	// imu raw sensor
+	this->sensor_ptr_ = gazebo::sensors::get_sensor(
+		this->model_->GetWorld()->Name() +
+		"::" + this->model_->GetScopedName() + "::" + this->imu_link_name_ +
+		"::" + this->imu_sensor_name_);
+	// imu sensor
+	if ( this->sensor_ptr_.get() == nullptr ) {
+		gzthrow("Could not find imu sensor !");
+	} else {
+		this->imu_sensor_ptr_ =
+			std::dynamic_pointer_cast<gazebo::sensors::ImuSensor>(
+				this->sensor_ptr_);
+	}
+	// imu publisher
+	this->imu_sensor_pub_ = ros2node_->create_publisher<sensor_msgs::msg::Imu>(
+		"imu_sensor", qos.get_publisher_qos("imu_sensor", rclcpp::QoS(10)));
 
 	// subscription
 	subscription_ =
@@ -88,6 +115,33 @@ void DronePlugin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 void DronePlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
 {
 	this->updateThrust();
+	this->publish_imu_sensor();
+}
+
+void DronePlugin::publish_imu_sensor()
+{
+
+	sensor_msgs::msg::Imu msg;
+	msg.header.stamp = ros2node_->now();
+	msg.header.frame_id = this->imu_sensor_ptr_->ScopedName();
+
+	msg.orientation.x = this->imu_sensor_ptr_->Orientation().X();
+	msg.orientation.y = this->imu_sensor_ptr_->Orientation().Y();
+	msg.orientation.z = this->imu_sensor_ptr_->Orientation().Z();
+	msg.orientation.w = this->imu_sensor_ptr_->Orientation().W();
+	msg.orientation_covariance.fill(1);
+
+	msg.angular_velocity.x = this->imu_sensor_ptr_->AngularVelocity().X();
+	msg.angular_velocity.y = this->imu_sensor_ptr_->AngularVelocity().Y();
+	msg.angular_velocity.z = this->imu_sensor_ptr_->AngularVelocity().Z();
+	msg.angular_velocity_covariance.fill(1);
+
+	msg.linear_acceleration.x = this->imu_sensor_ptr_->LinearAcceleration().X();
+	msg.linear_acceleration.y = this->imu_sensor_ptr_->LinearAcceleration().Y();
+	msg.linear_acceleration.z = this->imu_sensor_ptr_->LinearAcceleration().Z();
+	msg.linear_acceleration_covariance.fill(1);
+
+	this->imu_sensor_pub_->publish(msg);
 }
 
 // called each time receiving message from topic
