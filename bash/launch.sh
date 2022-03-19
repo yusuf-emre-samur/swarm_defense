c6edab201377b6fe3c982d4fe42a955a9e636d4e
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-03-19 12:31:42 +0100

    restructure

--- bash/launch.sh
+++ bash/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+export GAZEBO_MODEL_PATH=~/local/swarm_defense/models
 source /opt/ros/galactic/setup.bash
 source install/setup.bash
-export GAZEBO_MODEL_PATH=~/local/swarm_defense/models
 ros2 launch sd_simulation simulation.launch.py
