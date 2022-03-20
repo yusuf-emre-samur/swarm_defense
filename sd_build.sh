15ba02ecbf485866b3cebd0b7453b6acb3f36efa
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-03-20 13:20:01 +0100

    new executable build and launch file

---
+++ sd_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vars
+ROS_VERSION=galactic
+# source ros
+source /opt/ros/$ROS_VERSION/setup.bash
+# source local if exists
+FILE=$PWD/install/setup.bash
+if test -f "$FILE"; then
+    source install/setup.bash
+fi
+# build packages
+colcon build --symlink-install
