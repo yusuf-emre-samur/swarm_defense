b4f5394a61ea29a3eb168b74e7c4efedb006be2e
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-04-02 13:39:07 +0200

    added new interfaces

--- src/sd_flight_controller/src/sd_flight_controller.cpp
+++ src/sd_flight_controller/src/sd_flight_controller.cpp
@@ -7,18 +7,18 @@ FlightController::FlightController() : Node("default_node_name")
 {
 	// node params
 	// topic name of pose subscriper
-	this->declare_parameter<std::string>("pose_sub_topic_name", "");
-	this->get_parameter("pose_sub_topic_name", this->pose_sub_topic_name_);
+	this->declare_parameter<std::string>("pos_sub_topic_name", "");
+	this->get_parameter("pos_sub_topic_name", this->pos_sub_topic_name_);
 	// topic name of rpm publisher
-	this->declare_parameter<std::string>("pose_pub_topic_name", "");
-	this->get_parameter("pose_pub_topic_name", this->pose_pub_topic_name_);
+	this->declare_parameter<std::string>("pos_pub_topic_name", "");
+	this->get_parameter("pos_pub_topic_name", this->pos_pub_topic_name_);
 	// pid
 
 	// pose subscriber
-	this->pose_sub_ =
-		this->create_subscription<geometry_msgs::msg::PoseStamped>(
-			this->pose_sub_topic_name_, 1,
-			std::bind(&FlightController::on_pose_msg_callback, this,
+	this->pos_sub_ =
+		this->create_subscription<sd_interfaces::msg::Position3Stamped>(
+			this->pos_sub_topic_name_, 1,
+			std::bind(&FlightController::on_position_msg_callback, this,
 					  std::placeholders::_1));
 
 	// pid timer
@@ -27,8 +27,9 @@ FlightController::FlightController() : Node("default_node_name")
 		std::bind(&FlightController::flight_controller_timer_callback, this));
 
 	// rpm publisher
-	this->pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-		this->pose_pub_topic_name_, 10);
+	this->pos_pub_ =
+		this->create_publisher<sd_interfaces::msg::Position3Stamped>(
+			this->pos_pub_topic_name_, 10);
 
 	this->last_time_ = this->now();
 
@@ -45,33 +46,31 @@ void FlightController::flight_controller_timer_callback()
 }
 
 // callback for each received pose msg
-void FlightController::on_pose_msg_callback(
-	geometry_msgs::msg::PoseStamped::SharedPtr msg)
+void FlightController::on_position_msg_callback(
+	const sd_interfaces::msg::Position3Stamped::SharedPtr msg)
 {
 	// simply update last received msg
-	this->last_pose_ = msg;
+	this->last_pos_ = msg->pos3;
 }
 
-void FlightController::publish_goal_pos() const
+void FlightController::publish_goal_position() const
 {
-	geometry_msgs::msg::PoseStamped msg;
-	msg.pose.position.set__x(this->goal_pose_.position.x);
-	msg.pose.position.set__y(this->goal_pose_.position.y);
-	msg.pose.position.set__z(this->goal_pose_.position.z);
-	msg.pose.orientation.set__w(this->goal_pose_.orientation.w);
-	msg.pose.orientation.set__x(this->goal_pose_.orientation.x);
-	msg.pose.orientation.set__y(this->goal_pose_.orientation.y);
-	msg.pose.orientation.set__z(this->goal_pose_.orientation.z);
-
+	sd_interfaces::msg::Position3Stamped msg;
+	// header
 	msg.header.frame_id = this->get_name();
 	msg.header.stamp = this->now();
-	this->pose_pub_->publish(msg);
+	// pos3
+	msg.pos3.set__x(this->target_pos_.x);
+	msg.pos3.set__y(this->target_pos_.y);
+	msg.pos3.set__z(this->target_pos_.z);
+
+	this->pos_pub_->publish(msg);
 }
 
-void FlightController::set_goal_pos(geometry_msgs::msg::Pose::SharedPtr pos)
+void FlightController::set_goal_position(
+	const sd_interfaces::msg::Position3Stamped::SharedPtr pos)
 {
-	this->goal_pose_.position = pos->position;
-	this->goal_pose_.orientation = pos->orientation;
+	this->target_pos_ = pos->pos3;
 }
 
 } // namespace ros
