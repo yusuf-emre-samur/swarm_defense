f785f72b4fdf0e3910282a8d18cfb0eb195bac6d
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-05-27 21:06:05 +0200

    env variable

--- sd/gazebo_source.bash
+++ sd/gazebo_source.bash
@@ -1,7 +1,7 @@
-for d in $PWD/swarm_defense_models/* ; do
+for d in $PWD/swarm_defense_gazebo/models/* ; do
     if [ -d "$d" ]; then
         export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${d}
     fi
 done
-worlds=$PWD/worlds
-export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:${worlds}
+source /usr/share/gazebo/setup.bash
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:$PWD/swarm_defense_gazebo/worlds
