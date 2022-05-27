08f384e76b3921f4178e59a85b3dba658b17fac2
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-05-27 21:49:18 +0200

    new build script due to new pkgs

--- sd/build_sd.sh
+++ sd/build_sd.sh
@@ -3,6 +3,8 @@
 source $PWD/sd/ros_source.bash
 
 # build packages
-declare -a PACKAGES=(sd_simulation sd_interfaces sd_gazebo_ros_plugins sd_flight_controller sd_actor_controller sd_world_plugin)
+declare -a INTERFACES=(sd_interfaces)
+declare -a PLUGINS=(sd_actor_plugin sd_drone_plugin sd_world_plugin sd_gazebo_ros_plugins)
+declare -a PACKAGES=(sd_simulation sd_flight_controller sd_actor_controller)
 
-colcon build --symlink-install --packages-select ${PACKAGES[@]}
+colcon build --symlink-install --packages-select ${PACKAGES[@]} ${PLUGINS[@]} ${INTERFACES[@]}
