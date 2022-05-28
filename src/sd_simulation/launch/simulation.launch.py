a49e25f96a4103a0b67215c729f6ef4a486d955a
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-05-28 14:35:15 +0200

    rm sd_actor_controller node

--- src/sd_simulation/launch/simulation.launch.py
+++ src/sd_simulation/launch/simulation.launch.py
@@ -9,36 +9,24 @@ from launch_ros.actions import Node
 import launch
 
 
-def flight_controller_node(i):
-    return Node(
-        package="sd_flight_controller",
-        executable="sd_flight_controller",
-        name=f"sd_flight_controller_{i}",
-        parameters=[
-            {"id": f"sd_drone_{i}"},
-            {"use_sim_time": True}
-        ],
-        arguments=[
-
-        ],
-        namespace=f"sd_drone_{i}"
-    )
-
-
 def generate_launch_description():
-    # package share
-    pkg_share = FindPackageShare("sd_simulation")
+    # create launch desc.
+    ld = LaunchDescription()
 
-    # launch args
-
+    ###############################################################
+    # Launch arguments
+    ###############################################################
     # gazebo world file in worlds folder
     world_launch_arg = DeclareLaunchArgument(
         "world",
         default_value="world.world",
         description="Gazebo world file name in worlds folder of package."
     )
+    ld.add_action(world_launch_arg)
 
-    # start gazebo server
+    ###############################################################
+    # gazebo server
+    ###############################################################
     gzserver = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
@@ -48,20 +36,12 @@ def generate_launch_description():
             ]),
         ]),
         launch_arguments={
-            # PathJoinSubstitution([pkg_share, "worlds", LaunchConfiguration("world")]),
             "world": LaunchConfiguration("world"),
             "verbose": "true",
             "pause": "true"
         }.items()
     )
-    # create launch desc.
-    ld = LaunchDescription()
-    ld.add_action(world_launch_arg)
     ld.add_action(gzserver)
 
-    # add flight controller for each drone
-    num_drones = 1
-    for i in range(1, num_drones+1):
-        ld.add_action(flight_controller_node(i))
 
     return ld
