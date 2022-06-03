e3bd446392d60e60fa5c4e6ca34345ae77e9748b
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-06-03 16:38:21 +0200

    new pkg

--- sd/build_sd.sh
+++ sd/build_sd.sh
@@ -5,6 +5,6 @@ source $PWD/sd/ros_source.bash
 # build packages
 declare -a INTERFACES=(sd_interfaces)
 declare -a PLUGINS=(sd_actor_plugin sd_drone_plugin sd_world_plugin sd_gazebo_ros_plugins)
-declare -a PACKAGES=(sd_simulation sd_flight_controller sd_drone_controller sd_flight_controller sd_communication)
+declare -a PACKAGES=(sd_simulation sd_flight_controller sd_drone_controller sd_flight_controller sd_communication sd_visualiser)
 
 colcon build --symlink-install --packages-select ${PACKAGES[@]} ${PLUGINS[@]} ${INTERFACES[@]}
