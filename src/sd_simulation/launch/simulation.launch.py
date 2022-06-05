a7bd0fcce835fa2f05e0bb2f884e0e30caed27a0
yusuf-emre-samur <yusuf.emre.samur@icloud.com>
2022-06-05 21:26:56 +0200

    formatted launch file

--- src/sd_simulation/launch/simulation.launch.py
+++ src/sd_simulation/launch/simulation.launch.py
@@ -58,9 +58,9 @@ def generate_launch_description():
         [-102, -106, 0.175],
 
     ]
-    min_fly = 5
+    min_fly = 2
     battery = [100.0 for i in range(min_fly)] + \
-        [10.0 * i for i in range(num_drones-min_fly)]
+        [80.0 * i for i in range(num_drones-min_fly)]
 
     # pso
     pso_max_vel = [10.0, 10.0, 0.0]
@@ -70,6 +70,8 @@ def generate_launch_description():
 
     bbox_min = [-160.0, -160.0]
     bbox_max = [80.0, 80.0]
+
+    old_after = 10.0
 
 
     for i in range(num_drones):
@@ -81,17 +83,18 @@ def generate_launch_description():
                 executable="sd_drone_controller",
                 name="sd_drone_controller",
                 parameters=[
-                    {"use_sim_time": True},
-                    {"drone_id": i},
-                    {"base_station_pos": base_station_positions[i]},
-                    {"min_flying_drones": min_fly},
-                    {"battery": battery[i]},
-                    {"pso_w": pso_w},
-                    {"pso_c1": pso_c1},
-                    {"pso_c2": pso_c2},
-                    {"max_velocity": pso_max_vel},
-                    {"bbox_min": bbox_min},
-                    {"bbox_max": bbox_max}
+                    {
+                        "use_sim_time": True,
+                        "drone_id": i,
+                        "base_station_pos": base_station_positions[i],
+                        "min_flying_drones": min_fly,
+                        "battery": battery[i],
+                        "pso_w": pso_w,
+                        "pso_c1": pso_c1,
+                        "pso_c2": pso_c2,
+                        "max_velocity": pso_max_vel,
+                        "bbox_min": bbox_min,
+                        "bbox_max": bbox_max}
 
                 ],
                 arguments=[
@@ -110,8 +113,10 @@ def generate_launch_description():
                 executable="sd_flight_controller",
                 name="sd_flight_controller",
                 parameters=[
-                    {"use_sim_time": True},
-                    {"drone_id": i}
+                    {
+                        "use_sim_time": True,
+                        "drone_id": i
+                    }
                 ],
                 arguments=[
 
@@ -129,9 +134,11 @@ def generate_launch_description():
                 executable="sd_communication",
                 name="sd_communication",
                 parameters=[
-                    {"use_sim_time": True},
-                    {"drone_id": i},
-                    {"old_after": 10}
+                    {
+                        "use_sim_time": True,
+                        "drone_id": i,
+                        "old_after": old_after
+                    }
                 ],
                 arguments=[
 
@@ -148,7 +155,9 @@ def generate_launch_description():
             executable="sd_visualiser",
             name="sd_visualiser",
             parameters=[
-                    {"num_drones": num_drones}
+                    {
+                        "num_drones": num_drones
+                    }
             ],
             arguments=[
 
